220e7f2ae2594664b2ade515660a2e3d87502ba1
Daniel <frossdelaqrois@gmail.com>
2014-05-19 21:13:01 +0100

    Added Bot, Follower, Leader Prefab. Started Spawner Script

---
+++ GamesAIBasics-master/Assets/Dan/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Spawner : MonoBehaviour {
+
+	//float timePassed = 0.25f;
+	//GameObject ammo;
+
+	void Start()
+	{
+	
+	}
+
+
+	void Update()
+	{
+		Debug.DrawLine(transform.position, transform.position + transform.forward * 10.0f, Color.yellow);
+
+
+		float speed = 5.0f;
+		float width = 500;
+		float height = 500;
+		
+		if ((transform.position.x < -(width / 2)) || (transform.position.x > width / 2) || (transform.position.z < -(height / 2)) || (transform.position.z > height / 2) || (transform.position.y < 0) || (transform.position.y > 100))
+		{
+			Destroy(gameObject);
+		}
+		transform.position += transform.forward * speed;
+		Debug.DrawLine(transform.position, transform.position + transform.forward * 10.0f, Color.yellow);
+
+		//timePassed += Time.deltaTime;
+
+
+		//		if (timePassed > 0.25f) {
+		
+		//				GameObject ammo = new GameObject ();
+		//				ammo.AddComponent<Lazer> ();
+		//				ammo.transform.position = ammo.transform.position;
+		//				ammo.transform.forward = ammo.transform.forward;
+		//				timePassed = 0.0f;
+		//		}
+
+		}
+}
--- GamesAIBasics-master/Assets/GameManager.cs
+++ GamesAIBasics-master/Assets/GameManager.cs
@@ -5,14 +5,35 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		GameObject Bot = new GameObject ();
+		Bot.AddComponent<MeshRenderer> ();
+		Bot.AddComponent<MeshFilter> ();
+		Bot.AddComponent<StateMachine>();
+		Bot.AddComponent<SteeringBehaviours>();
+		Debug.DrawLine(transform.position, transform.position + transform.forward * 10.0f, Color.red);
+	
         GameObject leader = GameObject.FindGameObjectWithTag("leader");
         GameObject teaser = GameObject.FindGameObjectWithTag("teaser");
+		GameObject teaser2 = GameObject.FindGameObjectWithTag("teaser2");
+		GameObject teaser3 = GameObject.FindGameObjectWithTag("teaser3");
+		GameObject ammo = GameObject.FindGameObjectWithTag("pickup");
+
 
         leader.GetComponent<StateMachine>().SwitchState(new IdleState(leader, teaser));
         teaser.GetComponent<StateMachine>().SwitchState(new TeaseState(teaser, leader));
+		teaser2.GetComponent<StateMachine>().SwitchState(new TeaseState(teaser2, leader));
+		teaser3.GetComponent<StateMachine>().SwitchState(new TeaseState(teaser3, leader));
+		//ammo.GetComponent<StateMachine> ().SwitchState (new IdleState (ammo, leader));
+
 
         leader.renderer.material.color = Color.red;
         teaser.renderer.material.color = Color.blue;
+		teaser2.renderer.material.color = Color.blue;
+		teaser3.renderer.material.color = Color.blue;
+		Bot.renderer.material.color = Color.blue;
+		ammo.renderer.material.color = Color.yellow;
+
+
 
         
 	}
